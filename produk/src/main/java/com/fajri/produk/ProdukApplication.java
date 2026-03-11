c44db3f8e05204ddc749abe9f2da19e8c950348f
JFajri <jfnew24@gmail.com>
2026-03-11 11:02:50 +0700

    Add files via upload

--- produk/src/main/java/com/fajri/produk/ProdukApplication.java
+++ produk/src/main/java/com/fajri/produk/ProdukApplication.java
@@ -2,12 +2,14 @@ package com.fajri.produk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProdukApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProdukApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProdukApplication.class, args);
+    }
 
 }
