7b34928f6e2364a5b9c411c38201cdbbee3f2ddb
JFajri <jfnew24@gmail.com>
2026-07-25 15:36:30 +0700

    Add files via upload

--- consumer/src/main/java/com/consumer/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/consumer/consumer/ConsumerApplication.java
@@ -2,11 +2,13 @@ package com.consumer.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced; // Import ini
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsumerApplication {
 
     public static void main(String[] args) {
